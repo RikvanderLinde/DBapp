53045d18efa6093b81a4ad2f896acf0c405d9780
TheBlade7c <rik_7@msn.com>
2016-10-18 15:35:27 +0200

    dinsdag

--- Formapp/Formapp/StartPage.xaml.cs
+++ Formapp/Formapp/StartPage.xaml.cs
@@ -13,42 +13,53 @@ namespace Formapp
 {
     public partial class StartPage : ContentPage
     {
+
         public StartPage()
         {
             InitializeComponent();
-            
+
         }
 
         public class question
         {
+            [JsonProperty(PropertyName = "Question")]
             public string Question { get; set; }
+            [JsonProperty(PropertyName = "Type")]
             public string Type { get; set; }
+            [JsonProperty(PropertyName = "Info")]
             public string Info { get; set; }
+            [JsonProperty(PropertyName = "Answer1")]
             public string Answer1 { get; set; }
+            [JsonProperty(PropertyName = "Next1")]
             public string Next1 { get; set; }
+            [JsonProperty(PropertyName = "Answer2")]
             public string Answer2 { get; set; }
+            [JsonProperty(PropertyName = "Next2")]
             public string Next2 { get; set; }
+            [JsonProperty(PropertyName = "Answer3")]
             public string Answer3 { get; set; }
+            [JsonProperty(PropertyName = "Next3")]
             public string Next3 { get; set; }
         }
 
         public async Task<question> RefreshDataAsync(string url)
         {
-            HttpClient client;
-            client = new HttpClient();
+            HttpClient client = new HttpClient();
             client.MaxResponseContentBufferSize = 256000;
             Uri uri = new Uri("http://" + url);
+            //question item = new question();
+
             var response = await client.GetAsync(uri);
-            question item = new question();
 
             if (response.IsSuccessStatusCode)
             {
                 var content = await response.Content.ReadAsStringAsync();
-                
-                item = JsonConvert.DeserializeObject<question>(content);
+
+                Debug.WriteLine(content);
+                question item = JsonConvert.DeserializeObject<question>(content);
                 return item;
             }
-            return item;
+            return new question();
         }
 
         private void OnButtonClicked(object sender, EventArgs args)
@@ -58,16 +69,14 @@ namespace Formapp
 
         private async void GetData()
         {
-            //Dit is een waarde voor de database
-            string id = "1";
-            string url = $"172.20.42.202/app/question_get.php?ID={id}";
-            
-            question data = (await RefreshDataAsync(url));
-
-            //this.FindByName<Entry>("boxQuestion").Text = data.Question;
-            //boxQuestion.Text = data.Question;
-
-            Debug.WriteLine(data);
+            // UPDATE NIET, werkt soort van wel.
+            int id = 3;
+            string url = $"localhost/app/question_get.php?ID={id}";
+            question data = new question();
+            data = (await RefreshDataAsync(url));
+            this.dataLabel.Text = data.Question;
+            Debug.WriteLine(data.Next2);
+            Debug.WriteLine(data.Answer1);
         }
 
     }
